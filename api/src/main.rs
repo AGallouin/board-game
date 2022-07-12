fb99408ce706e7efafd63c92e4ac672984cd6c3c
AGallouin <gallouin.arthur@gmail.com>
2022-07-12 19:16:23 +0200

    async tokio

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,25 +2,26 @@ use std::env;
 use dotenv::dotenv;
 use mongodb::{options::ClientOptions, Client};
 
-[tokio::mongodb]
-async fn mongodb() -> mongodb::error::Result<()> {
-    let connectionUrl: String = format!("mongodb+srv://{}:{}@cluster0.cjovhvv.mongodb.net/?retryWrites=true&w=majority", login, password);
-    println!("{}", connectionUrl);
-
-    let client_options = ClientOptions::parse(connectionUrl).await?;
+async fn mongodb(login: &String, password: &String) -> mongodb::error::Result<()> {
+    println!("totototo");
+    let connection_url = format!("mongodb+srv://{}:{}@cluster0.cjovhvv.mongodb.net/?retryWrites=true&w=majority", login, password);
+    println!("{}", connection_url);
+    
+    let client_options = ClientOptions::parse(connection_url).await?;
     let client = Client::with_options(client_options)?;
     let database = client.database("testDB");
     Ok(())
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     dotenv().expect("Failed to read .env file");
     println!("Hello, world!");
     println!("{}", env::var("MONGO_LOGIN").expect("MONGO_LOGIN not found"));
 
     match (env::var("MONGO_LOGIN"), env::var("MONGO_PASSWORD")) {
         (Ok(login), Ok(password)) => {
-            mongoDB(login, password);
+            mongodb(&login, &password).await;
             println!("{}:{}", login, password);
         },
         _ => println!("Couldn't read env"),
