f056d69dbddc3b1810b85c4e46d3c38c36ba59ef
AGallouin <gallouin.arthur@gmail.com>
2022-07-12 11:53:56 +0200

    dotenv

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,8 @@
+use std::env;
+extern crate dotenv;
+
 fn main() {
+    dotenv::dotenv().expect("Failed to read .env file");
     println!("Hello, world!");
+    println!("{}", env::var("MONGO_LOGIN").expect("MONGO_LOGIN not found"));
 }
